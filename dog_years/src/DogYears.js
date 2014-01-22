400261bca55426d593ef7ca678edb7d59c93a60d
Kphillycat <keithwilliamsjr@gmail.com>
2014-01-22 09:01:43 -0500

    Add code to make tests pass

--- dog_years/src/DogYears.js
+++ dog_years/src/DogYears.js
@@ -1,2 +1,16 @@
 //create an object called dog
 //it should have 3 functions, ageInYears, ageInDays, ageInDogYears
+
+function Dog() {
+	this.age = 18,
+	this.ageInYears = function(){
+		return this.age;
+	},
+	this.ageInDays = function(){
+		return this.age * 365;
+	},
+	this.ageInDogYears = function(){
+		return this.age * 7;
+	};
+
+}
